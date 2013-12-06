1f078b5bf6ed5fb4382466bc5a07f5ad661f45bd
Lane Phillips <lane@milkllc.com>
2013-12-06 14:36:11 -0600

    Initial Commit

---
+++ URLTest/UTViewController.h
@@ -0,0 +1,13 @@
+//
+//  UTViewController.h
+//  URLTest
+//
+//  Created by Lane A. Phillips on 12/6/13.
+//  Copyright (c) 2013 Milk LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UTViewController : UIViewController
+
+@end
---
+++ URLTest/UTViewController.m
@@ -0,0 +1,29 @@
+//
+//  UTViewController.m
+//  URLTest
+//
+//  Created by Lane A. Phillips on 12/6/13.
+//  Copyright (c) 2013 Milk LLC. All rights reserved.
+//
+
+#import "UTViewController.h"
+
+@interface UTViewController ()
+
+@end
+
+@implementation UTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
