e253f637e25bbe98ec89a9b0f54742cf1321c73f
Lane Phillips <lane@milkllc.com>
2013-12-06 14:36:11 -0600

    Initial Commit

---
+++ URLTest/UTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UTAppDelegate.h
+//  URLTest
+//
+//  Created by Lane A. Phillips on 12/6/13.
+//  Copyright (c) 2013 Milk LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ URLTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  URLTest
+//
+//  Created by Lane A. Phillips on 12/6/13.
+//  Copyright (c) 2013 Milk LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UTAppDelegate class]));
+    }
+}
